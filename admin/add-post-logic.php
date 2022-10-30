e1ef60ff8c36780a815aca13b774255c50326715
farmingzone <qjawns0404@gmail.com>
2022-10-30 22:48:56 +0900

    221030

---
+++ admin/add-post-logic.php
@@ -0,0 +1,33 @@
+<?php
+require 'config/database.php';
+
+if(isset($_POST['submit'])) {
+    $author_id = $_SESSION['user-id'];
+    $title = filter_var($_POST['title'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $body = filter_var($_POST['body'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $catrgory_id = filter_var($_POST['category'], FILTER_SANITIZE_NUMBER_INT);
+    $is_featured = filter_var($_POST['is_featured'], FILTER_SANITIZE_NUMBER_INT);
+    $thumbnail = $_FILES['thumbnail'];
+
+    //set is_featured to 0 if unchecked
+    $is_featured = $is_featured == 1 ?: 0;
+
+    //validate form data
+    if (!$title) {
+        $_SESSION['add-post'] = "Enter post title";
+    } elseif (!$category_id) {
+        $_SESSION['add-post'] = "Select post category";
+    } elseif (!$body) {
+        $_SESSION['add-post'] = "Enter post body";
+    } elseif (!$thumbnail['name']) {
+        $_SESSION['add-post'] = "Choose post thumbnail";
+    } else {
+        //work on thumbnail
+        //rename the image
+        $time = time(); // make each image name unique
+        $thumbnail_name = $time . $thumbnail['name'];
+    }
+
+    var_dump($thumbnail);
+
+}
